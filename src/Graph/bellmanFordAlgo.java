fe7c33e5b78d87ab4b7f1c883c35f977ca3d490e
ARCHITA DEY <deyarchita26@gmail.com>
2025-06-30 23:03:31 +0530

    commit

--- src/Graph/bellmanFordAlgo.java
+++ src/Graph/bellmanFordAlgo.java
@@ -27,34 +27,73 @@ public class bellmanFordAlgo {
 
         graph[4].add(new Edge(4,1,-1));
     }
-    public static void bellMan(ArrayList<Edge> graph[], int src, int V){
-        int dist[]= new int[V];
-        for(int i=0;i<V;i++){
-            if(i!=src){
-                dist[i]=Integer.MAX_VALUE;
-            }
+//    public static void bellMan(ArrayList<Edge> graph[], int src, int V){
+//        int dist[]= new int[V];
+//        for(int i=0;i<V;i++){
+//            if(i!=src){
+//                dist[i]=Integer.MAX_VALUE;
+//            }
+//        }
+//        for(int i=0;i<V-1;i++){
+//            for(int j=0; j<graph[i].size();j++){
+//                Edge e= graph[i].get(j);
+//                int u=e.src;
+//                int v= e.dest;
+//                if(dist[u]!=Integer.MAX_VALUE && dist[u]+e.wt<dist[v]){
+//                    dist[v]= dist[u]+e.wt;
+//                }
+//            }
+//        }
+//        for(int i=0; i<V;i++){
+//            System.out.print(dist[i]+" ");
+//        }
+//        System.out.println();
+//    }
+//
+//    public static void main(String[] args) {
+//        int V=5;
+//        ArrayList<Edge> graph[]= new ArrayList[V];
+//        create(graph);
+//
+//        bellMan(graph,0,V);
+//    }
+public static ArrayList<Integer> bellmanFord(ArrayList<Edge> graph[], int src, int V) {
+    ArrayList<Integer> distances = new ArrayList<>(V);
+
+    // Initialize distances
+    for (int i = 0; i < V; i++) {
+        // distances.add(i==src ? 0: Integer.MAX_VALUE);
+        if(i==src){
+            distances.add(i);
+        }else{
+            distances.add(Integer.MAX_VALUE);
         }
-        for(int i=0;i<V-1;i++){
-            for(int j=0; j<graph[i].size();j++){
-                Edge e= graph[i].get(j);
-                int u=e.src;
-                int v= e.dest;
-                if(dist[u]!=Integer.MAX_VALUE && dist[u]+e.wt<dist[v]){
-                    dist[v]= dist[u]+e.wt;
-                }
-            }
-        }
-        for(int i=0; i<V;i++){
-            System.out.print(dist[i]+" ");
-        }
-        System.out.println();
     }
 
+    // Relax all edges V-1 times
+    for (int i = 0; i < V - 1; i++) {
+        for (int j = 0; j < graph[i].size(); j++) {
+            Edge e= graph[i].get(j);
+            int u=e.src;
+            int v=e.dest;
+            if(distances.get(u)!= Integer.MAX_VALUE && distances.get(u)+ e.wt<distances.get(v)){
+                distances.set(v, distances.get(u)+e.wt);
+            }
+        }
+    }
+    return distances;
+}
+
     public static void main(String[] args) {
-        int V=5;
-        ArrayList<Edge> graph[]= new ArrayList[V];
+        int V = 5;
+        ArrayList<Edge> graph[] = new ArrayList[V];
         create(graph);
 
-        bellMan(graph,0,V);
+        ArrayList<Integer> result = bellmanFord(graph, 0, V);
+            for (int i = 0; i < V; i++) {
+                System.out.print(result.get(i)+" ");
+            }
+            System.out.println();
+
     }
 }
